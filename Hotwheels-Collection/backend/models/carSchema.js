3c670e8cd8b5b68c6ad2a675869da168559d113a
Sharique Rahmani <skynight410@gmail.com>
2025-03-28 01:06:59 +0530

    Updated cars amount page

--- Hotwheels-Collection/backend/models/carSchema.js
+++ Hotwheels-Collection/backend/models/carSchema.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const carSchema = new mongoose.Schema({
+  id: Number,
   name: String,
   year: Number,
   series: String,
   image: String,
+  category: String,
+  price: String,
+  // Ensure this field exists
   ownedByUser: { type: Boolean, default: false }, // Ensure this field exists
   inCollection: { type: Boolean, default: false }, // Ensure this field exists
 });
